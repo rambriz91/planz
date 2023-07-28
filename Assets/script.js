0578e05604aa0f776166b81ae35152fbd09f6fa5
rambriz91 <rambriz91@gmail.com>
2023-07-28 11:39:34 -0700

    added a conditional statement to compare the hour with the current id and apply the classes: past present future; however, it is broken.

--- Assets/script.js
+++ Assets/script.js
@@ -1,21 +1,33 @@
 var today = dayjs();
 var time = parseInt(dayjs().format('H'));
+var timeBlock = $('.time-block');
 
 $(document).ready(function(){
 //Sets current date to the header element in January, 1, 2023 format
   $('#currentDay').text(today.format('[Today is] MMMM D, YYYY'));
 //Event listener that listens for clicks on the save button. Records the ID as as a key in local storage and adds the corresponding text content.
   $('.saveBtn').on('click', function() {
-    var timeBlock = $(this).parent().attr('id');
+    var hour = $(this).parent().attr('id');
     var blockText = $(this).siblings('textarea').val();
-    localStorage.setItem(timeBlock, blockText);
+    localStorage.setItem(hour, blockText);
   });
 // Grabs schedule from each hour key in local storage and displays it dynamically.
   function loadPlanz() {
     let i = 9;
-    $('.time-block').each(function() {
+    timeBlock.each(function() {
       $('#hour-'+[i]+ ' .description').val(localStorage.getItem('hour-'+[i]));
-      i++;
+        console.log(i);
+        console.log(time);
+        if (i === time) {
+        timeBlock.addClass('present')
+        }
+        else if (i < time) {
+        timeBlock.addClass('past');
+        }
+        else {
+        timeBlock.addClass('future')
+        }
+        i++;
     });
   }
 
@@ -28,13 +40,6 @@ $(document).ready(function(){
     // attribute of each time-block be used to conditionally add or remove the
     // past, present, and future classes? How can Day.js be used to get the
     // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-
-
   });
 
 
